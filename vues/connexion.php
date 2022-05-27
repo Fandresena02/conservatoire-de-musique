df0238feb4472417dfb0fe8b61f2fb98f83a662e
Fandresena02 <82474587+Fandresena02@users.noreply.github.com>
2022-05-27 15:15:46 +0200

    css

--- vues/connexion.php
+++ vues/connexion.php
@@ -2,15 +2,22 @@
 	<h2>Connexion</h2>
 </header><br>
 
-<div style="padding: 8%; margin-bottom: 10%; box-shadow: 0px 5px 18px grey; border-radius: 10px; background-color: #E7E7E7">
-<form action="index.php?action=validerConnexion" method = "POST">
+<div class="container">
+    <div class="row">
+	    <div class="col-2 col-12-medium"></div>
+            <div class="col-8 col-12-medium">
+                <div style="padding: 8%; margin-bottom: 10%; box-shadow: 55px 36px 18px grey; border-radius: 10px; background-color: #E7E7E7">
+                <form action="index.php?action=validerConnexion" method = "POST">
 
-    Identifiant :
-        <input type="text" name ="login" placeholder = "Entrez votre identifiant" required> <br>
-    Mot de passe : 
-        <input type="password" name ="mdp" placeholder = "Entrez votre mot de passe" required> <br>
-        <center> <input type="submit" name = "seconnecter" value="Se connecter"></center>
+                    Identifiant :
+                        <input type="text" name ="login" placeholder = "Entrez votre identifiant" required> <br>
+                    Mot de passe : 
+                        <input type="password" name ="mdp" placeholder = "Entrez votre mot de passe" required> <br><br>
+                        <center> <input type="submit" name = "seconnecter" value="Se connecter"></center>
 
 
-</form>
+                </form>
+                </div>
+            </div>
     </div>
+</div>
--- vues/inscrire.php
+++ vues/inscrire.php
@@ -1,51 +1,54 @@
 <br>
-<div style="padding: 8%; margin-bottom: 10%; box-shadow: 0px 5px 18px grey; border-radius: 10px; background-color: #E7E7E7">
+<div class="container">
+    <div class="row">
+            <div class="col-7 col-12-medium">
+                <div style="padding: 8%; margin-bottom: 10%; box-shadow: 25px 30px 18px grey; border-radius: 10px; background-color: #E7E7E7">
 
-    <form action="index.php?action=validerInscription" method = "post">
+                    <form action="index.php?action=validerInscription" method = "post">
 
-        <header class="style1">
+                        <header class="style1">
 
-            <h2>Réservation du cours n° <?php echo $numero;?></h2>
+                            <h2>Réservation du cours n° <?php echo $numero;?></h2>
 
-        </header>
+                        </header>
 
-        <input type="hidden" value="<?php echo $numero;?>" name="numero">
+                        <input type="hidden" value="<?php echo $numero;?>" name="numero">
 
-            Nom* : <input type="text" name="nom" placeholder = "Entrez le nom" required> <br>
-            Prénom* : <input type="text" name="prenom" placeholder = "Entrez le prénom" required> <br>
-            Tel* : <input type="text" name="tel" placeholder = "Entrez le n° tel" required> <br>
-            Adresse* : <input type="text" name="adresse" placeholder = "Entrez le adresse" required> <br>
-            Mail* : <input type="email" name="mail" placeholder = "Entrez le mail" required> <br><br>
-            <center><input type="submit" name = "save" value="S'inscrire"></center>
+                            Nom* : <input type="text" name="nom" placeholder = "Entrez le nom" required> <br>
+                            Prénom* : <input type="text" name="prenom" placeholder = "Entrez le prénom" required> <br>
+                            Tel* : <input type="text" name="tel" placeholder = "Entrez le n° tel" required> <br>
+                            Adresse* : <input type="text" name="adresse" placeholder = "Entrez le adresse" required> <br>
+                            Mail* : <input type="email" name="mail" placeholder = "Entrez le mail" required> <br><br>
+                            <center><input type="submit" name = "save" value="S'inscrire"></center>
 
-</form>
-</div>
+                    </form>
+                </div>
+            </div>
 
+            <div class="col-5 col-12-medium">
 
-<div style="padding: 8%; margin-bottom: 10%; box-shadow: 0px 5px 18px grey; border-radius: 10px; background-color: #E7E7E7">
-    <header class="style1">
-
-         <h2>Déjà inscrit?</h2>
-
-        </header>
-    <form action="index.php?action=validerInscription" method = "post">
-
-        <select name="inscription">
-            <?php foreach($lesAdherent as $unAdherent) 
-                    {
-                ?>
-            
-            <option value="<?php echo $unAdherent['idAdherent'];?>"><?php echo $unAdherent['idAdherent'],"- ",$unAdherent['nomAdherent']," ",$unAdherent['prenomAdherent'];?></option>
-            
-            <?php
-                        } 
-            ?>
-        </select>
-        <br><br>
-        <center><input type="submit" name = "sauvegarde" value="S'inscrire"></center>
-    </form>
-
-
+                        <div style="padding: 8%; margin-bottom: 10%; box-shadow: 25px 30px 18px grey; border-radius: 10px; background-color: #E7E7E7">
+                            <header class="style1">
+                                <h2>Déjà inscrit?</h2>
+                            </header>
+                            <form action="index.php?action=validerInscription" method = "post">
+                                <select name="inscription">
+                                    <?php foreach($lesAdherent as $unAdherent) 
+                                            {
+                                        ?>
+                                    
+                                    <option value="<?php echo $unAdherent['idAdherent'];?>"><?php echo $unAdherent['idAdherent'],"- ",$unAdherent['nomAdherent']," ",$unAdherent['prenomAdherent'];?></option>
+                                    
+                                    <?php
+                                                } 
+                                    ?>
+                                </select>
+                                <br><br>
+                                <center><input type="submit" name = "sauvegarde" value="S'inscrire"></center>
+                            </form>
+                        </div>
+            </div>
+    </div>
 </div>
 
 
